1bdd0130340484ca77b87e4d3e146dcf9b569166
Linghua Jin <ljin@walmartlabs.com>
2015-04-24 02:18:36 -0700

    loglevel

--- index.js
+++ index.js
@@ -10,61 +10,27 @@ let argv = yargs.default('host', 'localhost').argv; //if there is not host pass 
 let port = argv.port || 80;
 //bode index --host www.google.com --port 80
 let destUrl = argv.url || (argv.host === "localhost" ? "http://localhost:8000" : "http://" + argv.host + ":" + port);
-let logstream = argv.mylog ? fs.createWriteStream(argv.mylog) : process.stdout;
-let processInput =  process.stdin.read();
 
+//logger
+let logstream = argv.mylog ? fs.createWriteStream(argv.mylog) : process.stdout;
+let logLevelSetting = argv.loglevel || 'debug';
+let Logger = require('./logger');
+let myLog = new Logger(logLevelSetting, logstream);
+
+//child process
 let ChildProcess = require('./childProcess');
-let loggerLevelSetting = argv.loglevel || 'debug';
+
+
 if (argv.exec){
    ChildProcess.spawn(argv);
-}
-//TODO: log level
-// console.log("><loglevel", argv.loglevel);
-
-var Logger = {};
-Logger.log = function(){
-
-};
-
-
-Logger.logMsg = function(level, input){
-    // console.log(">< level", level);
-    // console.log("><input", input);
-    // console.log(">< inputinstance", typeof(input));
-    // console.log("input", input);
-    var logStr = "Logger[" + level + "]: ";
-
-    if (typeof(input) === 'string'){
-
-        console.log("\n >< is a string \n");
-       return logstream.write(logStr + '[string] ' +input);
-    }
-    //todo what is the best way for stream;
-    if (typeof(input) === 'object' && input.hasOwnProperty('readable')){
-       console.log("\n>< is a stream;\n")
-       logstream.write(logStr + '[stream]: ');
-       input.pipe(logstream);
-       // req.pipe(logstream); if we use pipe, logstream will end when the res is end. using through will keep the file from ending. 
-       // through(input, logstream, {
-       //     autoDestroy: false
-       // });
-
-       return;
-
-    }
-
-    return logstream.write(logStr + '[object] ' + JSON.stringify(input));
-
 }
 
 
 //set up the origin server
 http.createServer((req, res) => {
-    // Logger.logMsg("info" + JSON.stringify(req));
     for (let header in req.headers) {
         res.setHeader(header, req.headers[header]);
     }
-    // req.pipe(logstream);
     through(req, logstream, {
         autoDestroy: false
     });
@@ -77,29 +43,26 @@ logstream.write("listen to localhost:8000 \n");
 //set up the proxy server
 //curl -v http://127.0.0.1:8001 -d "niuniu" -H "x-asdf: foo"
 http.createServer((req, res) => {
-    // Logger.logMsg("info", "localhost:8001");
-    // Logger.logMsg("info", JSON.stringify(req));
-    // logstream.write(, req);
     let options = {
             headers: req.headers, //forward the header to dest server
             url: req.headers['x-destination-url'] || destUrl
         }
-        //make sure path and query param are forwarded.
+    //make sure path and query param are forwarded.
     options.url += req.url;
     //request(options) is a readable and writeable stream
     //then pipe the stream to response 
     var downStream = req.pipe(request(options));
-    // req.pipe(logstream);
-    Logger.logMsg("info", "request111");
-    Logger.logMsg("info", req);
+    myLog.debug("><request111");
+    myLog.info(req);
+    // through(downStream, logstream, {
+    //        autoDestroy: false
+    // });
+
     downStream.pipe(res);
-    Logger.logMsg("info", "downstream111");
-    Logger.logMsg("info", downStream);
+    
+    myLog.debug("><downstream111");
+    myLog.info(downStream);
 
-    // downStream.pipe(logstream);
-
-
-    // Logger.logMsg("info", "\n new request headers: \n" + JSON.stringify(req.headers));
 
 }).listen(8001);
 
--- logger.js
+++ logger.js
@@ -1,33 +1,72 @@
-let Logger = {}
-
-Logger.warn = function(){
-
+let logLevelLookup = {
+    'alert': 0,
+    'error': 1,
+    'warn': 2,
+    'info': 3,
+    'debug': 4
 }
 
-Logger.info = function(){
+class Logger {
+	/**
+	 * class logger
+	 * log input into logstream based on global log level.
+	 * 
+	 * @param  {[type]} loglevel  [description]
+	 * @param  {[type]} logstream [description]
+	 * @return {[type]}           [description]
+	 */
+    constructor(loglevel, logstream) {
+        this.logLevel = loglevel || 'debug';
+        this.logstream = logstream || process.stdout;
+    }
 
+    logMsg(level, input) {
+        if (logLevelLookup[level] > logLevelLookup[this.logLevel]) {
+            return;
+        }
+        var logStr = "Logger[" + level + "]: ";
+
+        if (typeof(input) === 'string') {
+            return this.logstream.write(logStr + '[string] ' + input);
+        }
+        //todo what is the best way for stream;
+        if (typeof(input) === 'object' && input.hasOwnProperty('readable')) {
+            this.logstream.write(logStr + '[stream]: ');
+            // if we use pipe, logstream will end when the res is end.
+            input.pipe(this.logstream);
+
+            //  using through will keep the file from ending.  this only need when log into a file
+            // through(input, logstream, {
+            //     autoDestroy: false
+            // });
+
+            return;
+
+        }
+
+        return this.logstream.write(logStr + '[object] ' + JSON.stringify(input));
+
+    }
+
+    alert(input) {
+        return this.logMsg('alert', input);
+    }
+
+    error(input) {
+        return this.logMsg('error', input);
+    }
+
+    warn(input) {
+        return this.logMsg('warn', input);
+    }
+
+    info(input) {
+        return this.logMsg('info', input);
+    }
+
+    debug(input) {
+        return this.logMsg('debug', input);
+    }
 }
 
-Logger.error = function(){
-
-}
-
-Logger.debug = function(){
-
-}
-
-Logger.log = function(level, input){
-	console.log(">< level");
-	console.log("instanceof(input)", instanceof(input));
-	if (instanceof(input)){
-
-	}
-}
-
-/**
- * write to logstream disregarding the log level
- * @return {[type]} [description]
- */
-function writeToLogSream(){
-
-}
+module.exports = Logger;
